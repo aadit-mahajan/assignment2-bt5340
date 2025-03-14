79ffa0cfed22542e3c128558190841027092c2c2
Aadit Mahajan <bs21b001@smail.iitm.ac.in>
2025-03-14 16:49:10 +0530

    corrected version

--- zimm_bragg.py
+++ zimm_bragg.py
@@ -1,181 +1,211 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import os
+import math
 
 plt.style.use('seaborn-paper')
 
+T_REF = 298
 SIGMA = 0.001
-R = 0.008314
+R = 8.314
+S_REF = 1.5
+NRES = 15
+TSTART = 200
+TEND = 450
+TSTEP = 20
 
-def partition_function(kappa, n=15):
+S_HI = 1.5
+S_LO = 0.5
 
-    # define the transfer matrix
-    mat = [
-        [1, SIGMA*kappa], 
-        [1, kappa]
-    ]
-    mat = np.array(mat)
+def get_s_temp(temp, s_ref=S_REF):
+    s = s_ref * math.exp((T_REF-temp)/temp)
+    return s
 
-    # eigendecomposition of the matrix
-    w, s = np.linalg.eig(mat)
-    s_inv = np.linalg.inv(s)
-
-    generator = s @ np.diag(np.power(w, n)) @ s_inv
-    pre_mult_mat = np.array([1, SIGMA*kappa])
-    post_mult_mat = np.array([1, 1])
-    partition_function = pre_mult_mat @ generator @ post_mult_mat
-
-    return partition_function
-
-def seq_sw(sequence, kappa):
-    nucleation = 0
-    n_helix = 0
-    if sequence[0] == 1:
-        nucleation += 1
- 
-    for i in range(1, len(sequence)):
-        if sequence[i] == 1 and sequence[i-1] == 0:
-            nucleation+=1
-
-    for i in sequence:
-        if i == 1:
-            n_helix += 1
-
-    sw = np.power(SIGMA, nucleation) * np.power(kappa, n_helix)
-
-    return sw
-
-def generate_sequences(n=15):
-    # generate all possible sequences of length n
-
-    seqs = []
-    for i in range(n):
-        for j in range(i, n):
-            if j-i >= 0:
-                seqs.append([1]*(j-i) + [0]*(n-(j-i)))
-            for k in range(j, n):
-                for l in range(k, n):
-                    if j-i >= 0 and k-j > 1 and l-k >= 0:
-                        seqs.append([1]*(j-i) + [0]*(k-j) + [1]*(l-k) + [0]*(n-(l-k)))
-    
-    return seqs
-
-def generate_sequences_ssa(n=15):
-    # generate all possible sequences of length n with only one nucleation site
-    seqs = []
-    for i in range(n):
-        for j in range(i, n):
-            if j-i >= 0:
-                seqs.append([1]*(j-i) + [0]*(n-(j-i)))
-
-    return seqs
-
-def generate_sequences_dsa(n=15):
-    # generate all possible sequences of length n with two nucleation sites
-    seqs = []
-    for i in range(n):
-        for j in range(i, n):
-            for k in range(j, n):
-                for l in range(k, n):
-                    if j-i >= 0 and k-j > 1 and l-k >= 0:
-                        seqs.append([1]*(j-i) + [0]*(k-j) + [1]*(l-k) + [0]*(n-(l-k)))
-
-    return seqs
-
-def free_energy(sequences, partition_function, kappa, temp, n=15):
-    free_energies = np.zeros(n)   # this stores the free energy of i helical residues at the ith index
-    z = partition_function
-
-    for seq in sequences:
-        n_helix = 0
-        for i in seq:
-            if i == 1:
-                n_helix += 1
+def get_all_sequences():
+    total_combs = 2**NRES
+    sequences = []
+    for i in range(total_combs):
+        bin_str = list(bin(i))[2:]
+        seq = (NRES-len(bin_str))*['0'] + bin_str
         
-        sw = seq_sw(seq, kappa)
-        if sw == 0:
-            sw = 1 
+        seq = [int(i) for i in seq]
+        sequences.append(seq)
 
-        prob = sw / z
-        free_energies[n_helix] += -R*temp*np.log(prob)
-    free_energies = free_energies/1000  # convert to kJ/mol
+    return sequences
+
+def get_nu_nhelix(seq):
+    num_nuc = 0
+    if seq[0] == 1:
+        num_nuc += 1
+    for i in range(1, len(seq)):
+        if seq[i] == 1 and seq[i-1] == 0:
+            num_nuc += 1
+
+    nhelix = sum(seq)
+    return num_nuc, nhelix
+
+def FE(temp, seqs):
+    z_tot = 0
+
+    s = get_s_temp(temp, S_REF)
+
+    sw_dict = {} 
+    for seq in seqs:
+        num_nuc, nhelix = get_nu_nhelix(seq)
+        sw = math.pow(SIGMA, num_nuc) * math.pow(s, nhelix)
+        # print(nhelix, num_nuc, sw, seq, math.pow(SIGMA, num_nuc), math.pow(s, nhelix), sep='\t')
+        z_tot += sw
+        if nhelix not in sw_dict.keys():
+            sw_dict[nhelix] = sw
+        else:
+            sw_dict[nhelix] += sw
+
+    probs = [sw_dict[i]/z_tot for i in range(NRES+1)]
+    # print('z_total', z_tot)
+    fe = -R*temp*np.log(probs)
+    # print(sw_dict, temp)
+    fe = fe/1000
+    return fe, probs
+
+def get_s_cont(seq, s_hi, s_lo, s1=3, e1=7, s2=10, e2=14):
+    s_cont = 1
+    helix_res = list(range(s1-1, e1)) + list(range(s2-1, e2))
+    for i in range(len(seq)):
+        if seq[i] == 1 and i in helix_res:
+            s_cont *= s_hi
+        elif seq[i] == 1 and i not in helix_res:
+            s_cont *= s_lo
+
+    return s_cont
+
+def FE_two_seq(temp, seqs):
+    z_tot = 0
+
+    s_hi = get_s_temp(temp, s_ref=S_HI)
+    s_lo = get_s_temp(temp, s_ref=S_LO)
+
+    sw_dict = {} 
+    for seq in seqs:
+        num_nuc, nhelix = get_nu_nhelix(seq)
+        s_cont = get_s_cont(seq, s_hi, s_lo)
+        sw = math.pow(SIGMA, num_nuc) * s_cont
+        # print(nhelix, num_nuc, sw, seq, math.pow(SIGMA, num_nuc), math.pow(s, nhelix), sep='\t')
+        z_tot += sw
+        if nhelix not in sw_dict.keys():
+            sw_dict[nhelix] = sw
+        else:
+            sw_dict[nhelix] += sw
+
+    probs = [sw_dict[i]/z_tot for i in range(NRES+1)]
+    # print('z_total', z_tot)
+    fe = -R*temp*np.log(probs)
+    # print(sw_dict, temp)
+    fe = fe/1000
+    return fe, probs
+
+def FE_s_var(temp, seqs):
     
-    return free_energies
+    fh_arr = []
+    for s in range(0, 6):
+        z_tot = 0
+        sw_dict = {} 
+        for seq in seqs:
+            num_nuc, nhelix = get_nu_nhelix(seq)
+            sw = math.pow(SIGMA, num_nuc) * math.pow(s, nhelix)
+            # print(nhelix, num_nuc, sw, seq, math.pow(SIGMA, num_nuc), math.pow(s, nhelix), sep='\t')
+            z_tot += sw
+            if nhelix not in sw_dict.keys():
+                sw_dict[nhelix] = sw
+            else:
+                sw_dict[nhelix] += sw
 
-def plot_free_energy(temps, free_energies, title, output_dir, type, kappa):
-    plt.figure()
+        probs = [sw_dict[i]/z_tot for i in range(NRES+1)]
+        # print('z_total', z_tot)
+        fe = -R*temp*np.log(probs)
+        # print(sw_dict, temp)
+        fe = fe/1000
+        fh = fraction_helicity(probs)
+        fh_arr.append(fh)
 
-    for i in range(len(temps)):
-        data = free_energies.iloc[:, i]
-        num_helices = np.arange(1, 16, 1)
-        plt.plot(num_helices, data, label=f'Temperature: {temps[i]} K', color=plt.cm.Reds(i/len(temps)))
-    plt.xlabel('Number of helical residues')
-    plt.ylabel('Free Energy (kJ/mol)')
-    plt.xticks(np.arange(1, 16, 1))
-    plt.legend()
-    plt.title(title)
+    plt.plot(range(6), fh_arr)
+    plt.title('Fraction Helicity vs s')
+    plt.xlabel('s')
     plt.grid()
-    plt.savefig(f'{output_dir}/{type}_kappa_{kappa :.3f}.png')
+    plt.ylabel(r'Fraction Helicity $\theta_H$')
+    plt.tight_layout()
+    plt.savefig('s_var.png')
+    plt.close()
+    # plt.show()
+
+def fraction_helicity(probs):
+    fh = [i*probs[i] for i in range(len(probs))]
+    return sum(fh)/NRES
+
+def plot_util(temp_range, seqs):
+    fh_arr = []
+    for t in temp_range:
+        fe, probs = FE(t, seqs)
+        fh_arr.append(fraction_helicity(probs))
+        plt.plot(fe, label=f"{t} K")
+    
+    plt.xlabel('Residue')
+    plt.ylabel('Free Energy (kJ/mol)')
+    plt.title('Free Energy vs Residue')
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig('folded_struct_var.png')
     plt.close()
 
+    # plt.show()
 
-def workflow(kappa, type = 'ssa'):
-    temp_min = 278
-    temp_max = 368
-    temp_step = 10
-    plot_output_dir = f'./fe_output/{type}'
-    os.makedirs(plot_output_dir, exist_ok=True)
-    z = partition_function(kappa)
-    
-    if type == 'ssa':
-        seq = generate_sequences_ssa()
-    elif type == 'dsa':
-        seq = generate_sequences_dsa()
-    elif type == 'all':
-        seq = generate_sequences()
+    plt.plot(temp, fh_arr)
+    plt.xticks(temp)
+    plt.xlabel('Temperature (K)')
+    plt.ylabel(r'Fraction Helicity $\theta_H$')
+    plt.title('Fraction Helicity vs Temperature')
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig('temp_var.png')
+    plt.close()
+    # plt.show()
 
-    fe_temps = {}
-    temps = np.arange(temp_min, temp_max+temp_step, temp_step)
-    for temp in temps:
-        fe = free_energy(seq, z, kappa, temp)
-        fe_temps[temp] = fe
+def plot_util_two_seq(temp_range, seqs):
+    fh_arr = []
+    for t in temp_range:
+        fe, probs = FE_two_seq(t, seqs)
+        fh_arr.append(fraction_helicity(probs))
+        plt.plot(fe, label=f"{t} K")
     
-    fe_df = pd.DataFrame(fe_temps)
-    plot_free_energy(temps, fe_df, f'Free Energy vs Number of Helical Residues ({type}), $\kappa = {kappa :.3f}$', plot_output_dir, type, kappa)
+    plt.xlabel('Residue')
+    plt.ylabel('Free Energy (kJ/mol)')
+    plt.title('Free Energy vs Residue (with 2 helical regions)')
+    plt.legend()
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig('folded_struct_var_two_seq.png')
+    plt.close()
+
+    # plt.show()
+
+    plt.plot(temp, fh_arr)
+    plt.xticks(temp)
+    plt.xlabel('Temperature (K)')
+    plt.ylabel(r'Fraction Helicity $\theta_H$')
+    plt.title('Fraction Helicity vs Temperature (with 2 helical regions)')
+    plt.grid()
+    plt.tight_layout()
+    plt.savefig('temp_var_two_seq.png')
+    plt.close()
 
 if __name__ == '__main__':
-    
-    kappa_values_low = np.arange(0.01, 0.11, 0.02)
-    kappa_values_high = np.arange(10, 60, 10)
 
-    for kappa in kappa_values_low:
-        workflow(kappa, 'ssa')
-        workflow(kappa, 'dsa')
-        workflow(kappa, 'all')
+   # Use the existing variables
 
-    for kappa in kappa_values_high:
-        workflow(kappa, 'ssa')
-        workflow(kappa, 'dsa')
-        workflow(kappa, 'all')
+    temp = range(TSTART, TEND+TSTEP, TSTEP)
+    sequences = get_all_sequences()
 
+    plot_util(temp, sequences)
+    plot_util_two_seq(temp, sequences)
 
-
-
-
-    
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
+    FE_s_var(298, sequences)
 
